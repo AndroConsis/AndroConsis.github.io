56e75b16149f185a303696ced1b439b715736a5e
Prateek Rathore <prateek@Prateeks-MacBook-Pro.local>
2020-06-16 01:07:59 +0530

    initial commit

---
+++ content/hero/index.md
@@ -0,0 +1,8 @@
+---
+title: 'Hi, my name is'
+name: 'Prateek Rathore'
+subtitle: 'I build things for the 💻 & 📱'
+buttonText: 'Get In Touch'
+---
+
+I'm a software engineer based in Pune, India specializing in building (and occasionally designing) exceptional, mobile applications, websites, and everything in between.
---
+++ src/config.js
@@ -0,0 +1,77 @@
+module.exports = {
+  siteTitle: 'Pragmatic',
+  siteDescription:
+    'Prateek Rathore is a software engineer based in Pune, India who specializes in building (and occasionally designing) exceptional websites, applications, and everything in between.',
+  siteKeywords:
+    'Prateek Rathore, Prateek, Rathore, androconsis, andro, consis, software engineer, front-end engineer, web developer, javascript, mobile app developer, react-native',
+  siteUrl: 'https://prateekrathore.com',
+  siteLanguage: 'en_US',
+  googleAnalyticsID: 'UA-45666519-2',
+  googleVerification: 'DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk',
+  name: 'Prateek Rathore',
+  location: 'Pune, India',
+  email: 'p.rathore.2903@gmail.com',
+  github: 'https://github.com/androconsis',
+  twitterHandle: '@socialprateek',
+  socialMedia: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/androconsis',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/prateek-rathore/',
+    },
+    {
+      name: 'Instagram',
+      url: 'https://www.instagram.com/socialprateek',
+    },
+    {
+      name: 'Twitter',
+      url: 'https://twitter.com/socialprateek',
+    },
+  ],
+
+  navLinks: [
+    {
+      name: 'About',
+      url: '/#about',
+    },
+    {
+      name: 'Experience',
+      url: '/#jobs',
+    },
+    {
+      name: 'Work',
+      url: '/#projects',
+    },
+    {
+      name: 'Contact',
+      url: '/#contact',
+    },
+  ],
+
+  navHeight: 100,
+
+  colors: {
+    green: '#64ffda',
+    navy: '#0a192f',
+    darkNavy: '#020c1b',
+  },
+
+  srConfig: (delay = 200) => ({
+    origin: 'bottom',
+    distance: '20px',
+    duration: 500,
+    delay,
+    rotate: { x: 0, y: 0, z: 0 },
+    opacity: 0,
+    scale: 1,
+    easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+    mobile: true,
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.25,
+    viewOffset: { top: 0, right: 0, bottom: 0, left: 0 },
+  }),
+};
