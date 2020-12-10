e69c961a2957d0a9e73828e2c34279d818337748
Prateek Rathore <prateek@Prateeks-MacBook-Pro.local>
2020-12-11 00:50:55 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a6ebfdceba782958396b.js"
+    "url": "webpack-runtime-94b8454017c8fe37c1d7.js"
   },
   {
     "url": "framework-f600a0dcdbbe2ab3be17.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "692c5366952995d584cda8051ed33df4"
+    "revision": "d47047ef97fe830d7362c11a3165f278"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7c31e2436cade51cbcda.js"
